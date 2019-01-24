7ba91064b9b2d6d44b8abe370c575b0e868c4974
Abram Van Der Geest <avanderg@tibco.com>
2019-01-24 12:26:44 -0500

    taking Samip's modified Inference activity to this repo

---
+++ activity/inference/activity_test.go
@@ -0,0 +1,312 @@
+package inference
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/project-flogo/contrib/activity/inference/framework/tf"
+	"github.com/project-flogo/core/activity"
+	"github.com/project-flogo/core/data/mapper"
+	"github.com/project-flogo/core/data/resolve"
+	"github.com/project-flogo/core/support/test"
+	"github.com/stretchr/testify/assert"
+)
+
+var _ tf.TensorflowModel
+
+/*
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}*/
+func TestRegister(t *testing.T) {
+
+	ref := activity.GetRef(&Activity{})
+	act := activity.Get(ref)
+
+	assert.NotNil(t, act)
+}
+
+func TestDNNEstimator(t *testing.T) {
+
+	var done bool
+	var err error
+
+	mf := mapper.NewFactory(resolve.GetBasicResolver())
+	iCtx := test.NewActivityInitContext(&Settings{}, mf)
+	act, err := New(iCtx)
+
+	tc := test.NewActivityContext(act.Metadata())
+
+	// Unit test of Estimator DNN Regressor model
+	fmt.Println("Unit test of Estimator Regressor model")
+	tc.SetInput("model", "testModels/Archive_estDNNrgr.zip")
+	tc.SetInput("inputName", "inputs")
+	var estInputsB = make(map[string]interface{})
+	estInputsB["one"] = 0.140586
+	estInputsB["two"] = 0.140586
+	estInputsB["three"] = 0.140586
+	estInputsB["label"] = 0.
+
+	var featuresB []interface{}
+	featuresB = append(featuresB, map[string]interface{}{
+		"name": "inputs",
+		"data": estInputsB,
+	})
+
+	tc.SetInput("framework", "Tensorflow")
+	tc.SetInput("sigDefName", "serving_default")
+	tc.SetInput("tag", "serve")
+	tc.SetInput("features", featuresB)
+
+	done, err = act.Eval(tc)
+	if done == false {
+		assert.Fail(t, fmt.Sprintf("Error raised: %s", err))
+	} else {
+		assert.True(t, done, fmt.Sprintf("Evaluation came back: %t", done))
+	}
+}
+
+/*
+func TestCreate(t *testing.T) {
+
+
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+func TestDNNEstimator(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	var done bool
+	var err error
+
+	// Unit test of Estimator DNN Regressor model
+	fmt.Println("Unit test of Estimator Regressor model")
+	tc.SetInput("model", "testModels/Archive_estDNNrgr.zip")
+	tc.SetInput("inputName", "inputs")
+	var estInputsB = make(map[string]interface{})
+	estInputsB["one"] = 0.140586
+	estInputsB["two"] = 0.140586
+	estInputsB["three"] = 0.140586
+	estInputsB["label"] = 0.
+
+	var featuresB []interface{}
+	featuresB = append(featuresB, map[string]interface{}{
+		"name": "inputs",
+		"data": estInputsB,
+	})
+
+	tc.SetInput("inputName", "inputs")
+	tc.SetInput("framework", "Tensorflow")
+	tc.SetInput("sigDefName", "serving_default")
+	tc.SetInput("tag", "serve")
+	tc.SetInput("features", featuresB)
+
+	done, err = act.Eval(tc)
+	if done == false {
+		assert.Fail(t, fmt.Sprintf("Error raised: %s", err))
+	} else {
+		assert.True(t, done, fmt.Sprintf("Evaluation came back: %t", done))
+	}
+}
+
+func TestEstimatorLinearRegressor(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	var done bool
+	var err error
+
+	// Unit test of Estimator Linear Regressor model
+	fmt.Println("Unit test of Linear Regressor Estimator model")
+	tc.SetInput("model", "testModels/Archive_LinReg.zip")
+	tc.SetInput("inputName", "inputs")
+	var estInputsC = make(map[string]interface{})
+	estInputsC["one"] = 0.140586
+	estInputsC["two"] = 0.140586
+	estInputsC["three"] = 0.140586
+	estInputsC["label"] = 0.
+
+	var featuresC []interface{}
+	featuresC = append(featuresC, map[string]interface{}{
+		"name": "inputs",
+		"data": estInputsC,
+	})
+
+	tc.SetInput("inputName", "inputs")
+	tc.SetInput("framework", "Tensorflow")
+	tc.SetInput("sigDefName", "serving_default")
+	tc.SetInput("tag", "serve")
+	tc.SetInput("features", featuresC)
+
+	done, err = act.Eval(tc)
+	if done == false {
+		assert.Fail(t, fmt.Sprintf("Error raised: %s", err))
+	} else {
+		assert.True(t, done, fmt.Sprintf("Evaluation came back: %t", done))
+	}
+
+}
+
+func TestPairwaiseMul(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	var done bool
+	var err error
+
+	// Unit test of Pairwaise Multiplication model
+	fmt.Println("Unit test of Pairwaise Multiplication model")
+	tc.SetInput("model", "testModels/Archive_pairwise_multi.zip")
+
+	var features2 []interface{}
+	features2 = append(features2, map[string]interface{}{
+		"name": "X1",
+		"data": [][]float32{{0.23, 4.5, -3.1}, {7.1, 3.14159, -0.00123}},
+	})
+	features2 = append(features2, map[string]interface{}{
+		"name": "X2",
+		"data": [][]float32{{4.34782608, 0.2222222222, -0.3225806451612903},
+			{0.14084507042253522, 0.31831015504887655, -813.0081300813008}},
+	})
+
+	fmt.Println(features2)
+
+	tc.SetInput("inputName", "inputs")
+	tc.SetInput("framework", "Tensorflow")
+	tc.SetInput("sigDefName", "serving_default")
+	tc.SetInput("tag", "serve")
+	tc.SetInput("features", features2)
+
+	done, err = act.Eval(tc)
+	if done == false {
+		assert.Fail(t, fmt.Sprintf("Error raised: %s", err))
+	} else {
+		assert.True(t, done, fmt.Sprintf("Evaluation came back: %t", done))
+	}
+}
+
+func TestCNNMOdel(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	var done bool
+	var err error
+
+	// Unit test ofSimple CNN model
+	fmt.Println("Unit test of simple CNN model")
+	tc.SetInput("model", "testModels/Archive_simpleCNN.zip")
+
+	var features3 []interface{}
+	features3 = append(features3, map[string]interface{}{
+		"name": "X",
+		"data": [][][][]float32{
+			{{{0.0000000856947568}}, {{0.00000331318370}}, {{0.0000858655563}}, {{0.00149167657}}, {{0.0173705094}}, {{0.135591557}}, {{0.709471493}}, {{2.48839579}}, {{5.85040827}}, {{9.22008867}}},
+			{{{9.22008867}}, {{5.85040827}}, {{2.48839579}}, {{00.709471493}}, {{0.135591557}}, {{0.00149167657}}, {{0.0000858655563}}, {{0.00000331318370}}, {{0.0000000856947568}}, {{0.}}},
+			{{{0.0173705094}}, {{0.135591557}}, {{0.709471493}}, {{2.48839579}}, {{5.85040827}}, {{9.22008867}}, {{5.85040827}}, {{2.48839579}}, {{0.709471493}}, {{0.135591557}}},
+		},
+	})
+
+	tc.SetInput("inputName", "inputs")
+	tc.SetInput("framework", "Tensorflow")
+	tc.SetInput("sigDefName", "serving_default")
+	tc.SetInput("tag", "serve")
+	tc.SetInput("features", features3)
+
+	done, err = act.Eval(tc)
+	if done == false {
+		assert.Fail(t, fmt.Sprintf("Error raised: %s", err))
+	} else {
+		assert.True(t, done, fmt.Sprintf("Evaluation came back: %t", done))
+	}
+
+	//check result attr
+	fmt.Println(tc.GetOutput("result"))
+}
+
+func TestEstimatorClassifier(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	var done bool
+	var err error
+
+	// Unit test of Estimator Classifier model
+	fmt.Println("Unit test of Estimator Classifier model")
+	tc.SetInput("model", "testModels/Archive_estDNNClf.zip")
+	tc.SetInput("inputName", "inputs")
+	var estInputsA = make(map[string]interface{})
+	estInputsA["one"] = 0.140586
+	estInputsA["two"] = 0.140586
+	estInputsA["three"] = 0.140586
+	estInputsA["label"] = 0
+
+	var featuresA []interface{}
+	featuresA = append(featuresA, map[string]interface{}{
+		"name": "inputs",
+		"data": estInputsA,
+	})
+
+	tc.SetInput("inputName", "inputs")
+	tc.SetInput("framework", "Tensorflow")
+	tc.SetInput("sigDefName", "serving_default")
+	tc.SetInput("tag", "serve")
+	tc.SetInput("features", featuresA)
+
+	done, err = act.Eval(tc)
+	if done == false {
+		assert.Fail(t, fmt.Sprintf("Error raised: %s", err))
+	} else {
+		assert.True(t, done, fmt.Sprintf("Evaluation came back: %t", done))
+	}
+}
+*/
---
+++ activity/inference/framework/tf/load.go
@@ -0,0 +1,150 @@
+package tf
+
+import (
+	"io/ioutil"
+	"strings"
+
+	models "github.com/project-flogo/contrib/activity/inference/model"
+	tfpb "github.com/project-flogo/contrib/activity/inference/tensorflow/tensorflow/core/protobuf"
+	"github.com/golang/protobuf/proto"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+)
+
+// Load implements the backend framework specifics for loading a saved model
+func (a *TensorflowModel) Load(model *models.Model, flags models.ModelFlags) (err error) {
+	var meta models.Metadata
+
+	meta.Tag = flags.Tag
+	meta.SigDef = flags.SigDef
+	modelFile := flags.ModelFile
+	modelPath := flags.ModelPath
+
+	// Parse the protobuffer
+	err = parseProtoBuf(modelFile, &meta)
+	if err != nil {
+		return err
+	}
+	model.Metadata = &meta
+
+	//Maybe add catch in case tag isn't in model
+	bundle, err := tf.LoadSavedModel(modelPath, []string{model.Metadata.Tag}, nil)
+	if err != nil {
+		return err
+	}
+	model.Instance = bundle
+
+	return nil
+}
+
+func parseProtoBuf(file string, model *models.Metadata) error {
+	savedModelPb, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	var savedModel tfpb.SavedModel
+	loadErr := proto.Unmarshal(savedModelPb, &savedModel)
+	if loadErr != nil {
+		return loadErr
+	}
+	metaGraphs := savedModel.GetMetaGraphs()
+
+	// Grab the default graph def
+	sigDef := metaGraphs[0].SignatureDef[model.SigDef]
+
+	// Collect inputs
+	inputs := getValues(sigDef.GetInputs())
+	outputs := getValues(sigDef.GetOutputs())
+	methodName := sigDef.GetMethodName()
+
+	// Determine the feature keys
+	if model.Inputs.Features == nil {
+		model.Inputs.Features = make(map[string]models.Feature)
+	}
+	var featureIndx = 0
+	for _, node := range metaGraphs[0].GetGraphDef().GetNode() {
+		// Grab dense features
+		if strings.Contains(node.GetName(), "ParseExample/ParseExample/dense_keys") {
+			k := string(node.GetAttr()["value"].GetTensor().GetStringVal()[0][:])
+
+			// Now determine the shape and type of each feature
+			for _, n2 := range metaGraphs[0].GetGraphDef().GetNode() {
+				if n2.GetName() == "ParseExample/ParseExample" {
+					var featureTyp string
+					var featureShape []int64
+
+					for attr, val := range n2.GetAttr() {
+
+						if attr == "Tdense" {
+							featureTyp = val.GetList().GetType()[featureIndx].String()
+						} else if attr == "_output_shapes" {
+							for i := 0; i < len(val.GetList().GetShape()[featureIndx].Dim); i++ {
+								featureShape = append(featureShape, val.GetList().GetShape()[featureIndx].Dim[i].GetSize())
+							}
+						}
+					}
+					feat := models.Feature{
+						Shape: featureShape,
+						Type:  featureTyp,
+					}
+
+					model.Inputs.Features[k] = feat
+				}
+			}
+
+			featureIndx++
+		}
+
+		op := node.Op
+		if strings.Contains(op, "Placeholder") {
+			var featureTyp string
+			var featureShape []int64
+			nName := node.GetName()
+
+			for attr, val := range node.GetAttr() {
+				if attr == "dtype" {
+					featureTyp = val.GetType().String()
+				} else if attr == "_output_shapes" {
+					for i := 0; i < len(val.GetList().GetShape()[0].Dim); i++ {
+						featureShape = append(featureShape, val.GetList().GetShape()[0].Dim[i].GetSize())
+					}
+
+				}
+			}
+
+			feat := models.Feature{
+				Shape: featureShape,
+				Type:  featureTyp,
+			}
+
+			model.Inputs.Features[nName] = feat
+		}
+	}
+
+	model.Inputs.Params = inputs
+	model.Outputs = outputs
+	model.Method = methodName
+
+	return nil
+}
+
+// Used to extract input and output ops and data from the singdef in the pb
+func getValues(sigDef map[string]*tfpb.TensorInfo) map[string]models.OperationParam {
+
+	params := make(map[string]models.OperationParam)
+	var i = 0
+	for key, ins := range sigDef {
+		var p models.OperationParam
+		p.Name = strings.Split(ins.GetName(), ":")[0]
+		p.Type = ins.GetDtype().String()
+
+		// grab the shape
+		for _, dim := range ins.GetTensorShape().GetDim() {
+			p.Shape = append(p.Shape, dim.GetSize())
+		}
+
+		params[key] = p
+		i++
+	}
+
+	return params
+}
